a6cf2e5fc4ee734fb2f14fc1bc9da29e197be355
Caio Merces <caiof.1994@gmail.com>
2021-12-27 01:35:19 -0300

    Add some other things

--- api/cassandra.py
+++ api/cassandra.py
@@ -1,3 +1,4 @@
+from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster
 from cassandra.query import ordered_dict_factory
 from flask import app
@@ -5,8 +6,13 @@ import os
 
 
 def connect_cassandra():
+    auth = PlainTextAuthProvider(
+        username=os.environ['CASSANDRA_USER'],
+        password=os.environ['CASSANDRA_PASS']
+    )
     cluster = Cluster([os.environ['CASSANDRA_HOST']],
-                      port=os.environ['CASSANDRA_PORT'])
+                      port=os.environ['CASSANDRA_PORT'],
+                      auth_provider=auth)
     session = cluster.connect(
         os.environ['CASSANDRA_DBNAME'], wait_for_all_pools=True)
     session.execute(f'USE {os.environ["CASSANDRA_DBNAME"]}')
