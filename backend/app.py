d0dfebde4da1090ca0b2f8c2a0a2449db6306d82
Caio Merces <caiof.1994@gmail.com>
2022-01-06 15:18:31 -0300

    Adjust in backend and frontend to enable receive of posts directly from backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from api.views import bp
 import os
 
 app = Flask(__name__)
 app.register_blueprint(bp)
+CORS(app)
 
 if __name__ == "__main__":
     app.run()
--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -15,23 +15,17 @@
 <script>
 import axios from "axios";
 
-const endpoint = "http://localhost:8000/";
+const endpoint = "http://localhost:5000";
 
 export default {
   data() {
     return {
-      posts: [
-        { title: "titulo", content: "teste de content", created_at:"2021-01-06 03:28:55", uid:"sdkgjdskgd-fdhsd-hdhdh5-fd" },
-        { title: "titulo 2", content: "teste de content2", created_at:"2021-01-06 01:04:41", uid:"sdkgjdskgd-fdhsd-hdhdh5-fd" },
-        { title: "titulo 3", content: "teste de content3", created_at:"2021-01-06 00:42:32", uid:"sdkgjdskgd-fdhsd-hdhdh5-fd" },
-        { title: "titulo 4", content: "teste de content4", created_at:"2021-01-05 023:18:25", uid:"sdkgjdskgd-fdhsd-hdhdh5-fd" },
-     
-      ],
+      posts: [],
     };
   },
   created() {
-    axios.get(endpoint + "api/posts/").then((response) => {
-      this.posts = response.data.data;
+    axios.get(endpoint + "/posts").then((response) => {
+      this.posts = response.data;
     });
   },
 };
